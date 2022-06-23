6fe5ca1d8567c061258d9b6449124ec62f97d1a3
Guthrie <71603173+tabiosg@users.noreply.github.com>
2022-06-23 15:43:34 -0400

    Add shebang

--- src/esw/raman/jetson_raman.sh
+++ src/esw/raman/jetson_raman.sh
@@ -1,1 +1,3 @@
+#!/bin/bash
+
 /usr/bin/socat tcp-listen:8888,reuseaddr,fork file:/dev/raman,nonblock,waitlock=/var/run/tty0.lock,b115200,raw,echo=0
--- src/esw/raman/science_raman.sh
+++ src/esw/raman/science_raman.sh
@@ -1,1 +1,3 @@
+#!/bin/bash
+
 /usr/bin/socat pty,link=/dev/ttyACM0,waitslave tcp:10.1.0.2:8888
